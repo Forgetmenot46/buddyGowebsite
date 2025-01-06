963f3753ebf92c853aed6096a0eeadcb4137e1db
Forgetmenot46 <wwe543216@hotmail.com>
2025-01-06 16:26:29 +0700

    commit จากอัด

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<?php
+include '../includes/db_config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>testing</p>
+</body>
+</html>
